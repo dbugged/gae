8b347ee440bec4c62fbb1cd82e1b2b6200c80c97
Shashidhar HB <hb.shashidhar@gmail.com>
2014-01-08 14:50:49 +0530

    Spring controller
    Added a simple spring controller to get started with Spring integration

--- kailarhomes/src/main/java/com/kailar/controller/SpringController.java
+++ kailarhomes/src/main/java/com/kailar/controller/SpringController.java
@@ -1,5 +1,23 @@
 package com.kailar.controller;
 
-public class SpringController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+
+@Controller
+@RequestMapping("/mailme")
+public class SpringController {
+	
+	@RequestMapping(value="/{emailId}", method = RequestMethod.GET)
+	public String getMailDetails(@PathVariable String emailId, ModelMap model) {
+ 
+		model.addAttribute("email", emailId);
+ 
+		//return to jsp page, configured in mvc-dispatcher-servlet.xml, view resolver
+		return "list";
+ 
+	}
 }
---
+++ kailarhomes/src/main/webapp/pages/list.jsp
@@ -0,0 +1,7 @@
+<html>
+<body>
+	<h1>Spring controller</h1>
+ 
+	<h3>E-mail ID : ${email}</h3>	
+</body>
+</html>
